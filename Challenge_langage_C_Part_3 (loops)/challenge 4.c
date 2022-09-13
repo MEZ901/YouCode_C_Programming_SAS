41ab547e3e95e438c2aa868e42dccbcf077a1d28
issam <issammez44@gmail.com>
2022-09-13 13:49:07 +0100

    editing challenge 4

--- Challenge_langage_C_Part_3 (loops)/challenge 4.c
+++ Challenge_langage_C_Part_3 (loops)/challenge 4.c
@@ -7,14 +7,19 @@ Write a C program that reads a series of positive integers less than 100 termina
 
 int main()
 {
-    int num, sum= 0, max=0;
-    printf("enter the number: "); scanf("%d", &num);
-    do {
-        sum+=num;
-        if(max<num) max = num;
-        printf("enter the number: "); scanf("%d", &num);
-    }while(num>0 && num<100);
-    printf("sum = %d\n", sum);
-    printf("max = %d\n", max);
+    int num, i, sum, max, k;
+    max = 0;
+    sum = 0;
+    i = 0;
+    printf("how many numbers you'll enter: "); scanf("%d", &k);
+    while(i<k){
+    	printf("enter the number: "); scanf("%d", &num);
+    	if(num == 0) break;
+    	else if(num < 0 || num > 100) continue;
+    	else{ sum+=num; if(max < num) max = num;}
+    	
+	}
+	printf("the sum of the numbers: %d\n", sum);
+	printf("the max of the numbers: %d", max);
     return 0;
 }
