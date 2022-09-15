a7441ce2de47894a7a4329d7e9a065a47d65097d
issam <issammez44@gmail.com>
2022-09-15 09:04:12 +0100

    editing challenge 2

--- Challenge_langage_C_Part_4 (functions)/challenge 2.c
+++ Challenge_langage_C_Part_4 (functions)/challenge 2.c
@@ -4,24 +4,22 @@ Create a swap() function to swap the value of a with the value of b, what do you
 
 #include <stdio.h>
 #include <stdlib.h>
-
-int swap(a, b){
-    int c;
-    c = a;
-    a = b;
-    b = c;
-    printf("a = %d\n", a);
-    printf("b = %d\n", b);
+int a, b;
+void swap (){
+    a = a+b;
+    b = a-b;
+    a = a-b;
 
 }
-
 int main()
 {
-    int a, b;
-    printf("enter the first number (a): "); scanf("%d", &a);
-    printf("enter the second number (b): "); scanf("%d", &b);
-    swap(a, b);
-    printf("the initial state of a = %d\n", a);
-    printf("the initial state of b = %d\n", b);
+
+    printf("enter a: "); scanf("%d", &a);
+    printf("enter b: "); scanf("%d", &b);
+    printf("the initial state of a: %d\n", a);
+    printf("the initial state of b: %d\n", b);
+    swap();
+    printf("the new state of a: %d\n", a);
+    printf("the new state of b: %d", b);
     return 0;
 }
